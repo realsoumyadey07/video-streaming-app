04342219d6b26fec5be7172ab5a4ac1eaa8f011f
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-04-28 16:20:47 +0530

    todo model added

---
+++ models/todos/todo.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+     {
+          content: {
+               type: String,
+               required: true
+          },
+          complete: {
+               type: Boolean,
+               default: false
+          },
+          createdby: {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: "User"
+          }
+     },
+     {
+          timestamps: true
+     }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
